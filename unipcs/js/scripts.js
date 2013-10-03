4b91e7aba018bdcbd2b0c99b5073f9ee570d144b
Zac Colley <hi@zaccolley.com>
2013-10-03 21:58:45 +0100

    Updated unipcs
    Script now autoupdates
    Commented code

--- unipcs/js/scripts.js
+++ unipcs/js/scripts.js
@@ -1,117 +1,135 @@
-var buildings = [];
+(function(){
+	var buildings = [];
+		
+	var geoLat = '';
+	var geoLon = '';
 
-var geoLat = '';
-var geoLon = '';
+	// get the geolocation
+	navigator.geolocation.getCurrentPosition(geoData, geoError);
 
-$.get('data.php', function(data){
-	var data = data.substring(1, data.length-1).split(',');
-	console.log(data);
+	// start main and run it every minute
+	main();
+	setInterval(main, 60000);
+})()
 
-	var lib = {
-		name: 'library',
-		avail: +data[0],
-		pcs: {
-			free: +data[1],
-			total: +data[2]
-		},
-		coords: {
-			lat: 50.792470,
-			lon: -1.098845
+function main(){
+	console.clear();
+	console.log('Update');
+
+	$.get('data.php', function(data){
+		// format the data and stick it in an array
+		var data = data.substring(1, data.length-1).split(',');
+		console.log(data);
+
+		var lib = {
+			name: 'library',
+			avail: +data[0],
+			pcs: {
+				free: +data[1],
+				total: +data[2]
+			},
+			coords: {
+				lat: 50.792470,
+				lon: -1.098845
+			}
+		};
+
+		var par = {
+			name: 'park',
+			avail: +data[4],
+			pcs: {
+				free: +data[5],
+				total: +data[6]
+			},
+			coords: {
+				lat: 50.797570,
+				lon: -1.094108
+			}
+		};
+
+		var por = {
+			name: 'portland',
+			avail: +data[8],
+			pcs: {
+				free: data[9],
+				total: data[10]
+			},
+			coords: {
+				lat: 50.798454,
+				lon: -1.099490
+			}
+		};
+
+		var ang = {
+			name: 'anglesea',
+			avail: +data[12],
+			pcs: {
+				free: +data[13],
+				total: +data[14]
+			},
+			coords: {
+				lat: 50.797709,
+				lon: -1.096582
+			}
+		};
+
+		buildings = [lib, par, por, ang];
+
+		for(var i = 0; i < buildings.length; i++){
+
+			var build = buildings[i];
+			console.log(build);
+
+			var id = build.name.substring(0, 3); // id created from first three chars
+
+			var avail = (build.avail) ? 'open' : 'closed'; // boolean for availability
+
+			$('#'+id).find('h1').html(build.name); // set the heading to the name of the building
+
+			var percent = (build.pcs.free / build.pcs.total) * 100; // get a percentage of pc availability
+			var percentPretty = Math.floor(percent)+'%'; // tidy the percentage and add a '%'
+
+			if(build.avail){ // if the building is open
+				var output = build.pcs.free +' / '+ build.pcs.total + ' available <span>('+percentPretty+')</span>';
+
+				var colourHue = percent * 0.8; // colours here for the bar
+				// using hsl so we can keep the tone the same and just change the hue
+				var colour = 'hsl('+colourHue+', 60%, 60%)';
+
+				$('#'+id).find('.percent').css('width', percentPretty);
+				$('#'+id).find('.percent').css('background', colour);
+			}else{ // if the building is closed
+				var output = 'Closed';
+			}
+
+			$('#'+id).find('p').html(output);
+
+			// if the gelocation is set
+			if(geoLat && geoLon){
+				geo(id, build)
+			}
 		}
-	};
-
-	var par = {
-		name: 'park',
-		avail: +data[4],
-		pcs: {
-			free: +data[5],
-			total: +data[6]
-		},
-		coords: {
-			lat: 50.797570,
-			lon: -1.094108
-		}
-	};
-
-	var por = {
-		name: 'portland',
-		avail: +data[8],
-		pcs: {
-			free: data[9],
-			total: data[10]
-		},
-		coords: {
-			lat: 50.798454,
-			lon: -1.099490
-		}
-	};
-
-	var ang = {
-		name: 'anglesea',
-		avail: +data[12],
-		pcs: {
-			free: +data[13],
-			total: +data[14]
-		},
-		coords: {
-			lat: 50.797709,
-			lon: -1.096582
-		}
-	};
-
-	buildings = [lib, par, por, ang];
-
-	for(var i = 0; i < buildings.length; i++){
-
-		var build = buildings[i];
-		console.log(build);
-
-		var id = build.name.substring(0, 3);
-
-		var avail = (build.avail) ? 'open' : 'closed';
-
-		$('#'+id).find('h1').html(build.name);
-
-		var percent = (build.pcs.free / build.pcs.total) * 100;
-		var percentPretty = Math.floor(percent)+'%';
-
-		if(build.avail){
-			var output = build.pcs.free +' / '+ build.pcs.total + ' available <span>('+percentPretty+')</span>';
-
-			var colourHue = percent * 0.8;
-			var colour = 'hsl('+colourHue+', 60%, 60%)';
-
-			$('#'+id).find('.percent').css('width', percentPretty);
-			$('#'+id).find('.percent').css('background', colour);
-		}else{
-			var output = 'Closed';
-		}
-
-		$('#'+id).find('p').html(output);
 
 		if(geoLat && geoLon){
-			build.distance = getDistanceFromLatLonInKm(geoLat, geoLon, build.coords.lat, build.coords.lon);
+
+			// working out which building has the smallest distance
+
+			var smallestD = buildings[0];
+
+			for(var j = 0; j < buildings.length; j++){
+				var build = buildings[j];
+				if(build.avail && build.distance < smallestD.distance){ smallestD = build; }
+			}
+
+			console.log(smallestD);
+
+			$('#'+smallestD.name.substring(0, 3)).find('h1').append('<span>(Closest)</span>');
+			
 		}
 
-		console.log('Current geo: ', geoLat, geoLon);
-		console.log('Distance: ', build.distance);
+	});
 
-		$('#'+id).find('h1').append('<span>' + build.distance.toString().substring(0, 4) + 'km');		
-	}
-
-	var smallestD = buildings[0];
-
-	for(var j = 0; j < buildings.length; j++){
-		var build = buildings[j];
-		if(build.avail && build.distance < smallestD.distance){ smallestD = build; }
-	}
-
-	console.log(smallestD);
-
-	$('#'+smallestD.name.substring(0, 3)).find('h1 span').append(' (Closest)');
-});
-
-navigator.geolocation.getCurrentPosition(geoData, geoError);
+}
 
 function geoData(pos){
 	geoLat = pos.coords.latitude;
@@ -119,6 +137,17 @@ function geoData(pos){
 }
 
 function geoError(){ console.log('No geolocation. :(');	}
+
+function geo(id, build){
+
+	build.distance = getDistanceFromLatLonInKm(geoLat, geoLon, build.coords.lat, build.coords.lon);
+
+	console.log('Current geo: ', geoLat, geoLon);
+	console.log('Distance: ', build.distance);
+
+	$('#'+id).find('p span').append(' &bull; ' + build.distance.toString().substring(0, 4) + 'km');	
+
+}
 
 // http://stackoverflow.com/questions/27928/how-do-i-calculate-distance-between-two-latitude-longitude-points
 
